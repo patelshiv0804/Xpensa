15ea8e9b6ee67a2c69e7837a6c654d43dab3cdb2
patelshiv0804 <patelshiv0804@gmail.com>
2025-04-15 23:31:08 +0530

    added some transition effects in the middle_functionality

--- frontend/src/component/Homepage_component/Middle_home_page.jsx
+++ frontend/src/component/Homepage_component/Middle_home_page.jsx
@@ -1,15 +1,22 @@
 import styles from '../../styles/Homepage_styles/middle_home_page.module.css'
+import { useEffect, useState } from 'react'
 
 function middle_home_page() {
+    const [visible, setVisible] = useState(false);
+
+    useEffect(() => {
+        setVisible(true);
+    }, []);
+
     return (
         <div className={styles.middle}>
             <div className={styles.firstline}>
-                <p className={styles.first_word}>Capture</p>
-                <p>, Categorize,</p>
+                <p className={`${styles.first_word} ${visible ? styles.animateHighlight : ''}`}>Capture</p>
+                <p className={`${styles.regular_text} ${visible ? styles.animateSlideIn : ''}`}>, Categorize,</p>
             </div>
             <div className={styles.secondline}>
-                <p>Conquer Your </p>
-                <p className={styles.second_word}>&nbsp;Budget</p>
+                <p className={`${styles.regular_text} ${visible ? styles.animateFadeIn : ''}`}>Conquer Your</p>
+                <p className={`${styles.second_word} ${visible ? styles.animateBounce : ''}`}>&nbsp;Budget</p>
             </div>
         </div>
     )
