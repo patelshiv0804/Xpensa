4f0f0a24add1fd430a3353e58fab5a9c28cbb4a7
patelshiv0804 <patelshiv0804@gmail.com>
2025-04-21 15:05:59 +0530

    absolte corrected path

--- backend/utils/saveProfileImages.js
+++ backend/utils/saveProfileImages.js
@@ -1,5 +1,3 @@
-const path = require('path');
-
 const saveProfileImages = (originalName) => {
     const timestamp = new Date().toISOString().replace(/[-:.]/g, '');
     return `${timestamp}_${originalName}`;
